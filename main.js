9c15f39aa07201a2481c5c8fb741da82fd725b83
Ali Jumani <159873336+alijumani94@users.noreply.github.com>
2024-05-26 12:23:11 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function greet(name, age) {
+    return "Hey ".concat(name, "! you are ").concat(age, " years old");
+}
+//let message= greet("Ali", 30);     //this is also the method to call function.
+//console.log(message);
+console.log(greet("Ali", 30));
---
+++ main.ts
@@ -0,0 +1,10 @@
+function greet (name: string, age: number): string{
+    
+
+   return `Hey ${name}! you are ${age} years old`; 
+}
+ //let message= greet("Ali", 30);     //this is also the method to call function.
+ //console.log(message);
+ 
+ console.log(greet("Ali", 28));
+ 
